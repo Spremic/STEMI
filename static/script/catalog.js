3a654cb223b339fa1165ad5c351fe1786c1af368
Djordje <dspremic1@gmail.com>
2023-10-16 11:21:55 +0200

    demo

--- static/script/catalog.js
+++ static/script/catalog.js
@@ -1,6 +1,6 @@
 let trenutnaPutanja = window.location.pathname;
 if (trenutnaPutanja === "/stolarija") {
   let catalog = document.querySelector("#catalog");
-  catalog.innerHTML = `<a href="test.docx" download="test.docx"><button>Catalog</button></a> 
+  catalog.innerHTML = `<a href="/catalog/paneli.pdf" download="/catalog/paneli.pdf"><button>Catalog</button></a> 
    `;
 }
