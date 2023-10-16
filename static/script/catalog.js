680231ecdfda13817baf53023647ead1a0887dba
Djordje <dspremic1@gmail.com>
2023-10-16 11:48:23 +0200

    demo

--- static/script/catalog.js
+++ static/script/catalog.js
@@ -1,6 +1,6 @@
 let trenutnaPutanja = window.location.pathname;
 if (trenutnaPutanja === "/stolarija") {
   let catalog = document.querySelector("#catalog");
-  catalog.innerHTML = `<a href="/catalog/paneli.pdf" download="/catalog/paneli.pdf"><button>Catalog</button></a> 
+  catalog.innerHTML = `<a href="/paneli.pdf" download="/paneli.pdf"><button>Catalog</button></a> 
    `;
 }
